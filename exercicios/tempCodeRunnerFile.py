2dab81844610cb77ab17aa034bf0f4d012cdf95e
Gean Marcos Figueredo Sena <geansena990@gmail.com>
2024-05-11 13:55:53 -0300

    commit

--- exercicios/tempCodeRunnerFile.py
+++ exercicios/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-Maior = 10 > 9  
+x = 5
+y = 10
+
+# Verifica se x é maior que 3 ou y é menor que 5
+if x > 3 or y < 5:
+    print("Pelo menos uma das condições é verdadeira.")
+else:
+    print("Ambas as condições são falsas.")
