bd58b81ce78fb75847cf2faae9bacfb52a82a2be
Gean Marcos Figueredo Sena <geansena990@gmail.com>
2025-06-08 12:21:11 -0300

    aulas python

---
+++ exercicios/Logica_de_programacao/print.py
@@ -0,0 +1,4 @@
+print(12, 34, 56, sep=" , " , end="\n")   # Imprime múltiplos valores separados por vírgula
+print(78, 90, 11, sep=" , " , end="\n")   # Imprime múltiplos valores separados por vírgula
+
+print()
